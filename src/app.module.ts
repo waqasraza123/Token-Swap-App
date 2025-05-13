867795a93ef15614cd187187d26aa9bebaf1ba04
Waqas Raza <waqasraza123@gmail.com>
2025-05-13 21:14:20 +0500

    config metamask

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Web3ConfigService } from './config/web3.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Web3ConfigService],
+  exports: [Web3ConfigService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/web3.config.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ethers } from 'ethers';
+
+@Injectable()
+export class Web3ConfigService {
+    private readonly provider: ethers.JsonRpcProvider;
+    private readonly wallet: ethers.Wallet;
+
+    constructor(private readonly configService: ConfigService) {
+        const rpcUrl = this.configService.get<string>('RPC_URL');
+        const privateKey = this.configService.get<string>('METAMASK_PRIVATE_KEY');
+
+        if (!rpcUrl || !privateKey) {
+            throw new Error('RPC_URL or METAMASK_PRIVATE_KEY is not defined in the .env file');
+        }
+
+        this.provider = new ethers.JsonRpcProvider(rpcUrl);
+        this.wallet = new ethers.Wallet(privateKey, this.provider);
+    }
+
+    getProvider(): ethers.JsonRpcProvider {
+        return this.provider;
+    }
+
+    getWallet(): ethers.Wallet {
+        return this.wallet;
+    }
+}
